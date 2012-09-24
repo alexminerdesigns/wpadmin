f80b2cc5cd22f6954fe4242f6ebb85f88a63d087
Alex Miner <alexminerdesigns@gmail.com>
2012-09-24 15:03:34 -0500

    Added mobile support, Added TinyMCE theming and support, Added additional button formatting

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -14,6 +14,7 @@
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
+	<?php print $scripts; ?>
   <script src="/<?php print drupal_get_path('theme', 'wpadmin'); ?>/js/modernizr.min.js"></script>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
@@ -22,5 +23,4 @@
   </div><?php print $page_top; ?>
 <?php print $page; ?>
 <?php print $page_bottom; ?>
-<?php print $scripts; ?>
 </body></html>
